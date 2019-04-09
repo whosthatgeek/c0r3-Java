4bae0fdfa51b748eccf6ad8db7813a83295ed20c
gprisch <gprisch@software-engineering.tech>
2019-04-09 19:45:55 +0200

    forgotten the break statement - 1 r34lly need t0 program again

--- welcome/src/leet/Leet.java
+++ welcome/src/leet/Leet.java
@@ -17,56 +17,82 @@ public class Leet {
                 case " ":
                     System.out.println("A");
                     l33t7.add(" ");
+                    break;
                 case "A":
                     l33t7.add("4");
+                    break;
                 case "C":
                     l33t7.add("8");
+                    break;
                 case "D":
                     l33t7.add("[)");
+                    break;
                 case "E":
                     l33t7.add("3");
+                    break;
                 case "F":
                     l33t7.add("/=");
+                    break;
                 case "G":
                     l33t7.add("6");
+                    break;
                 case "H":
                     l33t7.add("|-|");
+                    break;
                 case "I":
                     l33t7.add("1");
+                    break;
                 case "J":
                     l33t7.add("J_|");
+                    break;
                 case "K":
                     l33t7.add("|<");
+                    break;
                 case "L":
                     l33t7.add("|_");
+                    break;
                 case "M":
                     l33t7.add("|v|");
+                    break;
                 case "N":
                     l33t7.add("|\\||");
+                    break;
                 case "O":
                     l33t7.add("0");
+                    break;
                 case "P":
                     l33t7.add("|*");
+                    break;
                 case "Q":
                     l33t7.add("()_");
+                    break;
                 case "R":
                     l33t7.add("12");
+                    break;
                 case "S":
                     l33t7.add("5");
+                    break;
                 case "T":
                     l33t7.add("7");
+                    break;
                 case "U":
                     l33t7.add("|_|");
+                    break;
                 case "V":
                     l33t7.add("\\/");
+                    break;
                 case "W":
                     l33t7.add("\\/\\/");
+                    break;
                 case "X":
                     l33t7.add("><");
+                    break;
                 case "Y":
                     l33t7.add("`/");
+                    break;
                 case "Z":
                     l33t7.add("2 ");
+                    break;
             }
         }
         l33t7.forEach(System.out::print);
